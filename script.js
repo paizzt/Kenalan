75922c623151375786a38b9a1c6b7aa2149b06c7
sitiaisyah30 <aisyahsiti8363@gmail.com>
2024-05-31 23:07:03 +0800

    haloo

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('greetButton').addEventListener('click', function() {
+    var message = document.getElementById('greetMessage');
+    message.textContent = 'belum ada portofolio hehehe:) mau buat bareng bareng gak?';
+    message.style.display = 'block';
+});
